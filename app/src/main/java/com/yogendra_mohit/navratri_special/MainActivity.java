1e10c1068aa809c3210b06248e62c3db254d97fe
hitendratechnology <hitendratechnology@gmail.com>
2019-09-12 12:56:21 +0530

    Initial commit

---
+++ app/src/main/java/com/yogendra_mohit/navratri_special/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yogendra_mohit.navratri_special;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
